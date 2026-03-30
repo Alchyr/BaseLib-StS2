54a18c707fe180dbfb65537b667c95653608cd7a
Syra 3 <25936043+syralessthanthree@users.noreply.github.com>
2026-03-31 01:11:18 +0200

    Use ModHelper.SubscribeForRunStateHooks from the game to register any CustomSingletonModel we create.

---
+++ Abstracts/CustomSingletonModel.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Exceptions;
+using MegaCrit.Sts2.Core.Runs;
+
+namespace BaseLib.Abstracts;
+
+public abstract class CustomSingletonModel : SingletonModel {
+
+    public virtual SingletonSettings registerSettings { get; }
+    public virtual string modId { get; }
+    
+    protected CustomSingletonModel() {
+        if (modelsToRegister.ContainsKey(modId)) {
+            modelsToRegister[modId].Add(this);
+        } else {
+            modelsToRegister.Add(modId,[this]);
+        }
+    }
+
+    private static Dictionary<string, List<CustomSingletonModel>> modelsToRegister = []; 
+    public struct SingletonSettings {
+        
+    }
+    public static void Subscribe(string ModID)
+    {
+        var ourModels = modelsToRegister[ModID];
+        ModHelper.SubscribeForRunStateHooks(ModID,(_) => ourModels);
+    }
+}
