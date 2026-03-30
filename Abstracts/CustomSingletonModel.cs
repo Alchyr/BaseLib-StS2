a7d2c0f53c9567c20ae5ca1c49cff6e2a2840072
Syra 3 <25936043+syralessthanthree@users.noreply.github.com>
2026-03-31 01:36:07 +0200

    Add SubscribeToRunStateHooks and SubscribeToCombatStateHooks

--- Abstracts/CustomSingletonModel.cs
+++ Abstracts/CustomSingletonModel.cs
@@ -8,24 +8,36 @@ namespace BaseLib.Abstracts;
 
 public abstract class CustomSingletonModel : SingletonModel {
 
-    public virtual SingletonSettings registerSettings { get; }
-    public virtual string modId { get; }
-    
+    public override bool ShouldReceiveCombatHooks => registerSettings.SubscribeToCombatStateHooks;
+
+    public abstract SingletonSettings registerSettings { get; }
+    public abstract string modId { get; }
+
     protected CustomSingletonModel() {
         if (modelsToRegister.ContainsKey(modId)) {
-            modelsToRegister[modId].Add(this);
+            modelsToRegister[modId].Add(ModelDb.GetById<CustomSingletonModel>(Id));
         } else {
-            modelsToRegister.Add(modId,[this]);
+            modelsToRegister.Add(modId,[ModelDb.GetById<CustomSingletonModel>(Id)]);
         }
     }
-
+    
+    public static CustomSingletonModel GetSingletonId<T>(ModelId id) where T : CustomSingletonModel{
+        if (!ModelDb.Contains(typeof(T)))
+            ModelDb.Inject(typeof(T));
+        return ModelDb.GetByIdOrNull<T>(id) ?? throw new ModelNotFoundException(id);
+    }
+    
     private static Dictionary<string, List<CustomSingletonModel>> modelsToRegister = []; 
+    
     public struct SingletonSettings {
-        
+        public bool SubscribeToRunStateHooks;
+        public bool SubscribeToCombatStateHooks;
     }
     public static void Subscribe(string ModID)
     {
+        if(!modelsToRegister.ContainsKey(ModID)) return;
         var ourModels = modelsToRegister[ModID];
-        ModHelper.SubscribeForRunStateHooks(ModID,(_) => ourModels);
+        ModHelper.SubscribeForRunStateHooks(ModID,(_) => ourModels.FindAll((m)=> m.registerSettings.SubscribeToRunStateHooks));
+        ModHelper.SubscribeForCombatStateHooks(ModID,(_) => ourModels.FindAll((m)=> m.registerSettings.SubscribeToCombatStateHooks));
     }
 }
