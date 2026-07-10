04f139c5b2d19d7ecb0cf7a99ea0bc97d7a07730
quipsol <245758987+quipsol@users.noreply.github.com>
2026-07-10 15:51:17 +0200

    feature implemented
    first working version of the feature. Cleanup follows

---
+++ Abstracts/CustomLinkedRewardSet.cs
@@ -0,0 +1,191 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using BaseLib.Patches.Content;
+using BaseLib.Patches.Saves;
+using BaseLib.Utils;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Multiplayer.Serialization;
+using MegaCrit.Sts2.Core.Nodes.Rewards;
+using MegaCrit.Sts2.Core.Rewards;
+using MegaCrit.Sts2.Core.Saves.Runs;
+
+namespace BaseLib.Abstracts;
+
+/// <summary>
+/// We do not use BaseGame rewards because they are slightly buggy, and we are providing additional features which is too much work to patch in.
+/// </summary>
+public class CustomLinkedRewardSet : CustomReward
+{
+    // Do not rename, move, touch, anything to this field. We use it deterministically through the hash algorithm.
+    [CustomEnum] public static RewardType CustomLinkedRewardType;
+    private const string SaveId = "baselib_customlinkedrewardset_children";
+    
+    private static LocString HoverTipTitle => new LocString("static_hover_tips", "LINKED_REWARDS.title");
+    private static LocString HoverTipDesc => new LocString("static_hover_tips", "LINKED_REWARDS.description");
+
+    private static readonly SpireField<Reward, CustomLinkedRewardSet?> ParentCustomLinkedRewardSet = new(() => null);
+
+    public static bool TryGetCustomLinkedRewardSet(Reward reward, [NotNullWhen(true)] out CustomLinkedRewardSet? customLinkedRewardSet)
+    {
+        customLinkedRewardSet = ParentCustomLinkedRewardSet.Get(reward);
+        return customLinkedRewardSet != null;
+    }
+    
+    
+    private List<Reward> _rewards;
+    public IReadOnlyList<Reward> Rewards => _rewards.ToList();
+    
+    protected override RewardType RewardType  => CustomLinkedRewardType;
+    public override int RewardsSetIndex  => Rewards.Max((Reward r) => r.RewardsSetIndex);
+    public override CreateRewardFromSave<CustomReward> DeserializeMethod => CreateFromSerializable;
+    public override LocString Description => new LocString("gameplay_ui", "COMBAT_REWARD_LINKED");
+    public override bool IsPopulated  => _rewards.All((Reward r) => r.IsPopulated);
+
+    private LinkedRewardType _linkedRewardType;
+    public LinkedRewardType LinkedRewardType => _linkedRewardType;
+    
+    private CustomLinkedRewardSet() : base(null!)
+    {
+        _rewards = [];
+    }
+    public CustomLinkedRewardSet(List<Reward> rewards, Player player, LinkedRewardType linkedRewardType = LinkedRewardType.Exclusive) : base(player)
+    {
+        _rewards = rewards;
+        _linkedRewardType = linkedRewardType;
+        foreach (var reward in _rewards)
+            //reward.ParentRewardSet = this; // OLD
+            ParentCustomLinkedRewardSet.Set(reward, this);
+        
+    }
+
+    public override void Populate()
+    {
+        foreach (var reward in _rewards)
+            reward.Populate();
+        
+    }
+
+    protected override Task<bool> OnSelect()
+    {
+        return Task.FromResult(result: true);
+    }
+
+    public override void MarkContentAsSeen()
+    {
+        foreach (var reward in _rewards)
+            reward.MarkContentAsSeen();
+        
+    }
+    
+    public override void OnSkipped()
+    {
+        foreach (var reward in _rewards)
+            reward.OnSkipped();
+        
+    }
+
+    public void RemoveReward(Reward reward)
+    {
+        _rewards.Remove(reward);
+    }
+    
+    
+    // Called by the sideload setter below once the nested rewards have been
+    // reconstructed from save data.
+    internal void RestoreRewards(List<Reward> rewards, LinkedRewardType linkedRewardType)
+    {
+        _rewards = rewards;
+        _linkedRewardType = linkedRewardType;
+        foreach (var reward in _rewards)
+            ParentCustomLinkedRewardSet.Set(reward, this);
+    }
+
+    // --- CustomReward hookup ---
+
+    // Builds an empty placeholder; ExtendedSaveHandlers' existing postfix on
+    // Reward.FromSerializable calls our registered setter right after this returns,
+    // which is what actually fills in _rewards via RestoreRewards.
+    public static CustomReward CreateFromSerializable(SerializableReward save, Player player)
+        => new CustomLinkedRewardSet([], player);
+
+
+    public override void Initialize()
+    {
+        base.Initialize(); // registers DeserializeMethod against CustomLinkedRewardType
+        ExtendedSaveTypes.RegisterObjectSaveType<SerializableLinkedRewardData>(
+                    ExtendedSaveTypes.PropertyFunc<SerializableLinkedRewardData, List<SerializableReward>>(
+                                nameof(SerializableLinkedRewardData.Rewards)),
+                    ExtendedSaveTypes.PropertyFunc<SerializableLinkedRewardData, int>(
+                                nameof(SerializableLinkedRewardData.LinkedRewardTypeValue))
+        );
+        ExtendedSaveHandlers<Reward, SerializableReward>.RegisterSave<SerializableLinkedRewardData>(
+                    SaveId,
+                    getter: reward => reward is CustomLinkedRewardSet clrs
+                                ? new SerializableLinkedRewardData
+                                {
+                                            Rewards = clrs.Rewards.Select(r => r.ToSerializable()).ToList(),
+                                            LinkedRewardTypeValue  = (int)clrs.LinkedRewardType
+                                }
+                                : null,
+                    setter: (reward, value) =>
+                    {
+                        if (reward is not CustomLinkedRewardSet clrs || value == null) return;
+                        var restored = value.Rewards.Select(sr => Reward.FromSerializable(sr, reward.Player)).ToList();
+                        clrs.RestoreRewards(restored, (LinkedRewardType)value.LinkedRewardTypeValue );
+                    });
+    }
+}
+
+
+
+public enum LinkedRewardType
+{
+    None,
+    Exclusive,
+    Bundled
+}
+
+// Small wrapper so a List<SerializableReward> (+ LinkedRewardType) can ride along
+// as a sideloaded value via ExtendedSaveHandlers. Mirrors the pattern the base game
+// uses for CombatRoom.ExtraRewards.
+public class SerializableLinkedRewardData : IPacketSerializable
+{
+    public List<SerializableReward> Rewards { get; set; } = [];
+    public int LinkedRewardTypeValue { get; set; } // stored as int, not the enum directly
+
+
+    public void Serialize(PacketWriter writer)
+    {
+        writer.WriteList(Rewards);
+        writer.WriteInt(LinkedRewardTypeValue);
+    }
+
+    public void Deserialize(PacketReader reader)
+    {
+        Rewards = reader.ReadList<SerializableReward>();
+        LinkedRewardTypeValue = reader.ReadInt();
+    }
+}
+
+[HarmonyPatch]
+public static class CustomLinkedRewardSetPatches
+{
+    // TODO: Check what would happen. Eventually remove or replace with throwing an error
+    [HarmonyPatch(typeof(NRewardButton), "SetReward")]
+    [HarmonyPrefix]
+    private static void WarnOfUndefinedBehaviour(Reward reward)
+    {
+        if(reward is CustomLinkedRewardSet)
+            BaseLibMain.Logger.Warn($"The Reward inside an NRewardButton has been set to an object of \"CustomLinkedRewardSet\". This is undefined behaviour (base game would throw an error here). Proceed at your own risk.");
+    }
+
+    [HarmonyPatch(typeof(Reward), "HoverTips", MethodType.Getter)]
+    [HarmonyPostfix]
+    private static void AddHoverTip(Reward __instance, ref IEnumerable<IHoverTip> __result)
+    {
+        if (!CustomLinkedRewardSet.TryGetCustomLinkedRewardSet(__instance, out var customLinkedRewardSet)) return;
+        __result = [.. __result, .. customLinkedRewardSet.HoverTips];
+    }
+}
---
+++ BaseLibScenes/NCustomLinkedRewardSet.cs
@@ -0,0 +1,162 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using BaseLib.Abstracts;
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Assets;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Nodes.Rewards;
+using MegaCrit.Sts2.Core.Nodes.Screens;
+using MegaCrit.Sts2.Core.Rewards;
+
+namespace BaseLib.BaseLibScenes;
+
+[GlobalClass]
+public partial class NCustomLinkedRewardSet : Control
+{
+    public static readonly StringName RewardClaimedSignalName = "RewardClaimed";
+
+    [Signal]
+    public delegate void RewardClaimedEventHandler(NCustomLinkedRewardSet customLinkedRewardSet);
+
+    private NRewardsScreen _rewardsScreen;
+
+    private Control _rewardContainer;
+
+    private Control _chainsContainer;
+
+    public CustomLinkedRewardSet CustomLinkedRewardSet { get; private set; }
+
+    //private static string ScenePath => SceneHelper.GetScenePath("/rewards/linked_reward_set");
+    private static string ScenePath => $"res://BaseLib/scenes/linked_reward_set.tscn";
+
+    private static string ChainImagePath => ImageHelper.GetImagePath("/ui/reward_screen/reward_chain.png");
+
+    public static IEnumerable<string> AssetPaths => new string[2] { ScenePath, ChainImagePath };
+
+    private bool _grabbedReward = false;
+    private List<NRewardButton> rewardButtons = new();
+    
+    
+    public override void _Ready()
+    {
+        _rewardContainer = GetNode<Control>("%RewardContainer");
+        _chainsContainer = GetNode<Control>("%ChainContainer");
+        Reload();
+    }
+
+    public static NCustomLinkedRewardSet Create(CustomLinkedRewardSet linkedReward, NRewardsScreen screen)
+    {
+        var nCustomLinkedRewardSet = PreloadManager.Cache.GetScene(ScenePath).Instantiate<NCustomLinkedRewardSet>(PackedScene.GenEditState.Disabled);
+        nCustomLinkedRewardSet._rewardsScreen = screen;
+        nCustomLinkedRewardSet.SetReward(linkedReward);
+        return nCustomLinkedRewardSet;
+    }
+
+    private void SetReward(CustomLinkedRewardSet linkedReward)
+    {
+        CustomLinkedRewardSet = linkedReward;
+        if (IsNodeReady())
+        {
+            Reload();
+        }
+    }
+
+    private void Reload()
+    {
+        if (!IsNodeReady())
+        {
+            return;
+        }
+        rewardButtons.Clear();
+        for (var i = 0; i < CustomLinkedRewardSet.Rewards.Count; i++)
+        {
+            var reward = CustomLinkedRewardSet.Rewards[i];
+            var nRewardButton = NRewardButton.Create(reward, _rewardsScreen);
+            rewardButtons.Add(nRewardButton);
+            nRewardButton.CustomMinimumSize -= Vector2.Right * 20f;
+            _rewardContainer.AddChildSafely(nRewardButton);
+            nRewardButton.Connect(NRewardButton.SignalName.RewardClaimed, Callable.From<NRewardButton>(GetReward));
+            if (i >= CustomLinkedRewardSet.Rewards.Count - 1) continue;
+            var textureRect = new TextureRect();
+            textureRect.MouseFilter = MouseFilterEnum.Ignore;
+            textureRect.Texture = PreloadManager.Cache.GetCompressedTexture2D(ChainImagePath);
+            textureRect.Size = Vector2.One * 50f;
+            _chainsContainer.AddChildSafely(textureRect);
+            textureRect.GlobalPosition = _chainsContainer.GlobalPosition + Vector2.Down * i * (3f + nRewardButton.CustomMinimumSize.Y);
+        }
+    }
+
+    private void GetReward(NRewardButton button)
+    {
+        if (CustomLinkedRewardSet.LinkedRewardType == LinkedRewardType.Bundled)
+        {
+            if (_grabbedReward) return;
+            _grabbedReward = true;
+            rewardButtons.Remove(button);
+            foreach (var rewardButton in rewardButtons)
+            {
+                rewardButton.GetReward();
+            }
+        }
+        //_rewardsScreen.RewardCollectedFrom(this);
+        CustomLinkedRewardSet.OnSkipped();
+        EmitSignal(RewardClaimedSignalName, this);
+        this.QueueFreeSafely();
+    }
+}
+
+[HarmonyPatch]
+public static class NCustomLinkedRewardSetPatches
+{
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(NRewardsScreen), "_Ready")]
+    private static IEnumerable<CodeInstruction> ReplaceConnectMethodWithCustom(IEnumerable<CodeInstruction> instructions, MethodBase originalMethod)
+    {
+        var customLinkedRewardSetCheck = AccessTools.Method(typeof(NCustomLinkedRewardSetPatches), nameof(CustomLinkedRewardSetCheck));
+        var nRewardButtonCreate = AccessTools.Method(typeof(NRewardButton), nameof(NRewardButton.Create));
+
+        var matcher = new CodeMatcher(instructions)
+                    // Anchor on the unique call that starts the `else` branch:
+                    // option = NRewardButton.Create(item, this);
+                    .MatchStartForward(new CodeMatch(OpCodes.Call, nRewardButtonCreate))
+                    .ThrowIfInvalid("Could not find NRewardButton.Create call (else-branch start)");
+
+        // Step back to the else-branch's real first instruction: ldloc.s V_7
+        matcher.Advance(-3);
+
+        var optionField = (FieldInfo)matcher.InstructionAt(4).operand; // grab the real field, no guessing
+        var endLabel = matcher.InstructionAt(-1).operand; // the preceding br.s's target = IL_037f
+
+        // This instruction is the actual jump target for the `if` check (item is LinkedRewardSet == false).
+        // Detach its label so we can move it onto our own first instruction instead.
+        var elseEntryLabels = new List<System.Reflection.Emit.Label>(matcher.Labels);
+        matcher.Labels.Clear();
+
+        var loadDisplayClass = matcher.Instruction.Clone().WithLabels(elseEntryLabels); // ldloc.s V_7 (now the real entry point)
+        var loadRewardItem = matcher.InstructionAt(1).Clone(); // ldloc.s reward
+
+        var newInstructions = new List<CodeInstruction>
+        {
+                    loadDisplayClass, // push V_7
+                    new CodeInstruction(OpCodes.Ldflda, optionField), // push &V_7.option
+                    loadRewardItem, // push reward
+                    new CodeInstruction(OpCodes.Ldarg_0), // push this
+                    new CodeInstruction(OpCodes.Call, customLinkedRewardSetCheck),
+                    new CodeInstruction(OpCodes.Brtrue, endLabel), // handled -> skip the rest of the else-branch
+        };
+
+        matcher.Insert(newInstructions); // insert before the (now unlabeled) original else-branch code
+
+        return matcher.InstructionEnumeration();
+    }
+
+
+    private static bool CustomLinkedRewardSetCheck(ref Control option, Reward item, NRewardsScreen screen)
+    {
+        if (item is not CustomLinkedRewardSet clrs) return false;
+        option = NCustomLinkedRewardSet.Create(clrs, screen);
+        option.Connect(NCustomLinkedRewardSet.RewardClaimedSignalName, Callable.From<NCustomLinkedRewardSet>(screen.RewardCollectedFrom));
+        return true;
+    }
+}
